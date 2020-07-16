5da8a3d6c4b5c2669790bc5c97f99224a48a18cf
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-07-16 15:29:22 -0700

    got the tests passing at mvp level.  Now I am trying to think of how it can be done better

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -12,10 +12,10 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-map_file = "maps/test_cross.txt"
+# map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -31,7 +31,9 @@ player = Player(world.starting_room)
 traversal_path = []
 visited_rooms = {}
 
+#place to store the reversed path for backtracking
 back_track_path = []
+#establish the directions of the backward traversal
 backwards_traversal = {"n":"s", "e":"w", "s":"n", "w":"e"}
 
 #get that first room with it's exits into 'visited'
@@ -39,12 +41,32 @@ visited_rooms[player.current_room.id] = player.current_room.get_exits()
 
 #figure out loop for visited list vs list of 500 rooms
 while len(visited_rooms) < len(room_graph):
-    if player.current_room.id not in visited_rooms:
-        visited_rooms[player.current_room.id] = player.current_room.get_exits()
+    if player.current_room.id not in visited_rooms:  #if not in the visited, add to visited_rooms
+        #use the get_exits() from the Room class (n,s,e,w)
+        visited_rooms[player.current_room.id] = player.current_room.get_exits() #get the exit information
         route_to_current_room = back_track_path[-1]
+        #take the route_to_current_room and remove (using the built-in .remove() )
         visited_rooms[player.current_room.id].remove(route_to_current_room)
 
-    if len(visited_rooms[player.current_room.id]) == 0:  #When all paths have been explored
+    #When all paths have been explored, we need to backtrack to a room with unexplored exits
+    if len(visited_rooms[player.current_room.id]) == 0:  #deadend
+        #engage backtracking :)
+        route_to_current_room = back_track_path[-1]
+        back_track_path.pop()
+        traversal_path.append(route_to_current_room)
+        #by using the .travel() method on the player class, move the player
+        player.travel(route_to_current_room)
+
+    else:
+        #grab a direction
+        cardinal_dir = visited_rooms[player.current_room.id][-1]
+        visited_rooms[player.current_room.id].pop()
+        #add this direction/path to the traversal_path
+        traversal_path.append(cardinal_dir)
+        #add the backwards traversal
+        back_track_path.append(backwards_traversal[cardinal_dir])
+        #move the player again using the .travel() from the player class
+        player.travel(cardinal_dir)
 
 #figure out what to do when you hit a deadend. a room where all paths have been discovered.
 #backtrack until you encounter a room with paths that have not been discovered
