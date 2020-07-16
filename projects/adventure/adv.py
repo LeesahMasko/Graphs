3778af5d92fa98b2f0fa2274263471c13cc38288
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-07-16 14:12:14 -0700

    Getting some code written, I think I am sorta on the right track...

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,6 +1,7 @@
 from room import Room
 from player import Player
 from world import World
+from util import Queue, Stack
 
 import random
 from ast import literal_eval
@@ -11,10 +12,10 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -28,10 +29,25 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
-visited_rooms = {}  # all the room IDs that have been visited in the traversal
+visited_rooms = {}
 
 back_track_path = []
-opposite_dir = {"n":"s", "e":"w", "s":"n", "w":"e"}
+backwards_traversal = {"n":"s", "e":"w", "s":"n", "w":"e"}
+
+#get that first room with it's exits into 'visited'
+visited_rooms[player.current_room.id] = player.current_room.get_exits()
+
+#figure out loop for visited list vs list of 500 rooms
+while len(visited_rooms) < len(room_graph):
+    if player.current_room.id not in visited_rooms:
+        visited_rooms[player.current_room.id] = player.current_room.get_exits()
+        route_to_current_room = back_track_path[-1]
+        visited_rooms[player.current_room.id].remove(route_to_current_room)
+
+    if len(visited_rooms[player.current_room.id]) == 0:  #When all paths have been explored
+
+#figure out what to do when you hit a deadend. a room where all paths have been discovered.
+#backtrack until you encounter a room with paths that have not been discovered
 
 
 # TRAVERSAL TEST
